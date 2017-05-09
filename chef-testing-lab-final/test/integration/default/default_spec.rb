4a41dedb90ffdc902b2e744dc07b45cd48684e5d
Winslow Dalpe <winslow.dalpe@apexlearning.com>
2017-05-09 13:24:05 -0700

    Making inspec tests use new rspec assertion style
    Inspec's docs all use an old form of rspec assertions (it, should, ...)
    rather than the new style of expect().to(...)
    
    We can write inspec tests in the new style, and it would make sense to
    do so in order to teach a consistent assertion style between ChefSpec
    and Inspec tests.

--- chef-testing-lab-final/test/integration/default/default_spec.rb
+++ chef-testing-lab-final/test/integration/default/default_spec.rb
@@ -1,21 +1,30 @@
-expected_timezone = attribute('expected_tz', {})
+expected_timezone = attribute("expected_tz", {})
 
-describe file('/etc/sysconfig/clock') do
-    expected_content = %Q(ZONE="#{expected_timezone}"
+describe ("/etc/sysconfig/clock tests") do
+    it("should have correct content") do
+        f = file("/etc/sysconfig/clock")
+        expected_content = %Q(ZONE="#{expected_timezone}"
 UTC=true)
-    it { should exist }
-    it { should be_file }
-    its('content') { should match(expected_content) }
+        expect(f).to(exist())
+        expect(f).to(be_file())
+        expect(f.content).to(match(expected_content))
+    end
 end
 
-describe file('/etc/localtime') do
-    it { should exist }
-    it { should be_file }
-    it { should be_symlink }
-    it { should be_linked_to("/usr/share/zoneinfo/#{expected_timezone}")}
+describe("/etc/localtime tests") do
+    it("Should set up /etc/localtime correctly") do
+        f = file("/etc/localtime")
+        expect(f).to(exist())
+        expect(f).to(be_file())
+        expect(f).to(be_symlink())
+        expect(f).to(be_linked_to("/usr/share/zoneinfo/#{expected_timezone}"))
+    end
 end
 
-describe service('ntpd') do
-    it { should be_installed }
-    it { should be_enabled }
+describe("ntpd service") do
+    it("should be installed and enabled") do
+        s = service("ntpd")
+        expect(s).to(be_installed())
+        expect(s).to(be_enabled())
+    end
 end
--- chef-testing-lab/test/integration/default/default_spec.rb
+++ chef-testing-lab/test/integration/default/default_spec.rb
@@ -1,17 +1,20 @@
 # Unfortunately, Inspec requires you to define any attributes you expect to use
 # during a test run. (remember these are provided values externally)
-expected_timezone = attribute('expected_tz', {})
+expected_timezone = attribute("expected_tz", {})
 
 # We use an rspec-like syntax to write our tests.
 # Here we describe a file
-describe file('/etc/sysconfig/clock') do
-    # We expect it to have some certain content, depending on our test attribute
-    expected_content = %Q(ZONE="#{expected_timezone}"
+describe ("/etc/sysconfig/clock tests") do
+    it("should have correct content") do
+        f = file("/etc/sysconfig/clock")
+        # We expect it to have some certain content, depending on our test attribute
+        expected_content = %Q(ZONE="#{expected_timezone}"
 UTC=true)
-    # followed by some additional matchers
-    it { should exist }
-    it { should be_file }
-    its('content') { should match(expected_content) }
+        # followed by some additional matchers
+        expect(f).to(exist())
+        expect(f).to(be_file())
+        expect(f.content).to(match(expected_content))
+    end
 end
 
 # TODO:
@@ -22,7 +25,7 @@ end
 #    * is a symlink
 #    * is symlinked to the expected timezone file in /usr/share/zoneinfo
 
-describe('test /etc/localtime') do
+describe("test /etc/localtime") do
     pending(%Q(Write a test which verifies
 /etc/localtime 
 * exists, 
@@ -32,7 +35,10 @@ describe('test /etc/localtime') do
 end
 
 # Here we describe a service and its expected state
-describe service('ntpd') do
-    it { should be_installed }
-    it { should be_enabled }
+describe "ntpd service" do
+    it("should be installed and enabled") do
+        s = service("ntpd")
+        expect(s).to(be_installed())
+        expect(s).to(be_enabled())
+    end
 end
